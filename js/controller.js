add7001c8d30956c12158dbec4bb62118ccff0fa
manan5439 <manan5439@yahoo.com>
2018-06-19 03:41:58 +0530

    change

--- js/controller.js
+++ js/controller.js
@@ -14,8 +14,12 @@ $(document).ready(function(){
 $(document).keypress(function(e) {
     if(e.which == 13) {
       var allSelects = document.getElementsByTagName("span");
+      var pselects = document.getElementsByClassName("single-line") ;
+      var lastpselect = pselects[pselects.length-1];
       var lastSelect = allSelects[allSelects.length-1];
-      $(lastSelect).hide();
+      $(lastSelect).remove();
+      $(lastpselect).remove();
+
       var element = "<div class=\"remove\" style=\"margin-top:20;\">C:\\Person\\Information&gt  <p class=\"single-line\" id=\"input\" contenteditable=\"true\"></p> <span class=\"blink_me\">_</span></div>"
         $( ".new_container" ).append( element );
         allSelects = document.getElementsByTagName("p");
@@ -24,6 +28,7 @@ $(document).keypress(function(e) {
     }
     if(e.which == 113){
       $(".remove").remove();
+
     }
 });
 
