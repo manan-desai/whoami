b094774220170a1fd356ed6a2cb5611e5587f2bd
manan5439 <manan5439@yahoo.com>
2018-06-21 13:25:44 +0530

    update

--- js/controller.js
+++ js/controller.js
@@ -1,38 +1,76 @@
 
 $(document).ready(function(){
-  var allSelects = document.getElementsByTagName("p");
-  var lastSelect = allSelects[allSelects.length-1];
-  lastSelect.focus();
+  $( ".new_container" ).append( element );
 
+    last_select(".single-line").focus();
     $("#main_container").click(function(){
-
-      lastSelect.focus();
-
+      last_select("p").focus();
 });
 });
 var element = "<div class=\"remove\" style=\"margin-top:20;\">C:\\Person\\Information&gt  <p class=\"single-line\" id=\"input\" contenteditable=\"true\"></p> <span class=\"blink_me\">_</span></div>"
 $(document).keypress(function(e) {
     if(e.which == 13) {
-      var allSelects = document.getElementsByTagName("span");
-      var pselects = document.getElementsByClassName("single-line") ;
-      var lastpselect = pselects[pselects.length-1];
-      var lastSelect = allSelects[allSelects.length-1];
-      $(lastSelect).remove();
-      $(lastpselect).remove();
-
-
-        $( ".new_container" ).append( element );
-        allSelects = document.getElementsByTagName("p");
-        lastSelect = allSelects[allSelects.length-1];
-        lastSelect.focus();
-    }
-    if(e.which == 113){
-      $(".remove").remove();
+      var p_value = $(last_select(".single-line")).text();
+      $(last_select(".blink_me")).remove();
+      $(last_select(".single-line")).remove();
       $( ".new_container" ).append( element );
+        last_select(".single-line").focus();
+        commands(p_value);
+
     }
 });
 
+function last_select(select){
+  if(select[0]=="."){
+  var allSelects = document.getElementsByClassName(select.slice(1));
+}
+else{
+  var allSelects = document.getElementsByTagName(select);
+}
+  return allSelects[allSelects.length-1];
+}
 
+var ob = {
+  firstname:"manan",
+  lastname:"desai",
+  study:{
+    college:"bapu",
+    hsc:"r.c patel",
+    ssc:"c.m patel"
+  }
+}
+
+function commands(value){
+  var values = value.split(" ");
+
+  if(value=="clear"){
+    $(".remove").remove();
+    $( ".new_container" ).append( element );
+    last_select("p").focus();
+  }
+  else if(typeof(ob[values[0]])=="string"){
+    alert(ob[values[0]]);
+  }
+
+  else if(typeof(ob[values[0]])=="object"){               //if obj
+    if(typeof(ob[values[0]][values[1]])=="string"){
+      alert(ob[values[0]][values[1]]);
+    }
+    else {
+      alert(Object.keys(ob[values[0]]))
+    }
+  }
+
+  else if(value==""){
+  }
+
+  else{
+    alert(Object.keys(ob))
+ }
+
+
+
+}
 
 
 //var allSelects = document.getElementsByTagName("p");
